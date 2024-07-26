55544f816be2e86b3b35e471090349fd99c8f396
bitrojas <bitbucket.org.01@gmail.com>
2024-07-26 19:00:43 -0400

    project init
    Se inicia el proyecto, se habilitan variables de entorno, se aplica endpoint home y proyecto levanta

---
+++ server.js
@@ -0,0 +1,38 @@
+/* ENVIRONMENT */
+const dotenv = require("dotenv").config();
+
+/* LOGS */
+const log4 = require("log4js");
+const logger = log4.getLogger("server.js");
+logger.level = 'all';
+
+/* SERVER */
+const express = require("express");
+
+/* APP */
+const app = express();
+
+/* PARSER */
+const parser = require("body-parser");
+
+/* CORS */
+const cors = require("cors");
+
+/* ROUTES */
+const { home } = require("./src/routes");
+
+/* PARSER CONFIG */
+app.use(parser.urlencoded({ extended: false }));
+app.use(parser.json());
+
+/* CORS CONFIG */
+app.use(cors());
+
+/* ROUTER CONFIG */
+const base_path = '/api/' + process.env.API_VERS;
+app.use(base_path + '/home', home);
+
+/* STATICS FOLDERS */
+app.use(express.static('uploads'));
+
+module.exports = app;
