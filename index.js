6e0f216e80bb70ac8a952a6d080835dd1766d3ba
bitrojas <bitbucket.org.01@gmail.com>
2024-07-27 00:53:58 -0400

    Connect to BDD
    Se establece la conexion a la base de datos diferenciando ambientes de acuerdo al usuario de cada ambiente

--- index.js
+++ index.js
@@ -16,14 +16,39 @@ const app = require('./server');
 var PORT = undefined;
 var URI = undefined;
 
+/*
+mongodb+srv://<username>:<password>@imperial.ffnbucj.mongodb.net/?retryWrites=true&w=majority&appName=imperial
+
+mongodb+srv://dev_usr_imp@imperial.ffnbucj.mongodb.net/
+*/
 PORT = process.env.API_PORT;
 
 (async () => {
     try {
+        switch (process.env.NODE_ENV) {
+            case 'prd':
+                URI = `mongodb+srv://prd_${process.env.DB_USER}:prd_${process.env.DB_PASS}@imperial.ffnbucj.mongodb.net/`
+                //logger.error(URI);
+                break;
+            case 'dev':
+                URI = `mongodb+srv://dev_${process.env.DB_USER}:dev_${process.env.DB_PASS}@imperial.ffnbucj.mongodb.net/`
+                //logger.info(URI);
+                break;
+            case 'tst':
+                URI = `mongodb+srv://tst_${process.env.DB_USER}:tst_${process.env.DB_PASS}@imperial.ffnbucj.mongodb.net/`
+                //logger.trace(URI);
+                break;
+        }
+        await mongo.connect(URI);
+        logger.debug(`Connect to BDD ${process.env.NODE_ENV}_${process.env.DB_NAME}`);
         await app.listen(PORT, () => {
             logger.debug("Server it's Running.... in PORT " + PORT + ", en Ambiente de: " + process.env.NODE_ENV);
         });
     } catch (error) {
-        logger.fatal("Server is Not Running...");
+        let response = {
+            "Title": "Error",
+            "Message":error.message
+        }
+        logger.fatal(response);
     }
 })();
